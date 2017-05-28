c130324e235c95265fed98aeea7fb8b61894b095
keansafari <khjafari@cpp.edu>
2017-05-27 21:24:12 -0700

    Tic Tac Toe Server/Client with GUI
    Tic tac toe client which communicates with a server

---
+++ TicTacToeClient.java
@@ -0,0 +1,18 @@
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import javax.swing.JPanel;
+import javax.swing.JFrame;
+
+public class TicTacToeClient extends JFrame{
+
+
+    public static void main(String[] args) {
+        MenuGUI m = new MenuGUI();
+        m.setVisible(true);
+    }
+
+
+}
